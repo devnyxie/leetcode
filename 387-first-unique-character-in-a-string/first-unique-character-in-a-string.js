ec927e0f8133b9060bbc75e448daf1a0dc958f55
Tim Afanasiev <84281349+devnyxie@users.noreply.github.com>
2024-08-04 12:09:36 +0200

    Time: 85 ms (77.96%) | Memory: 52.5 MB (89.02%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -2,22 +2,13 @@
  * @param {string} s
  * @return {number}
  */
+
+
 var firstUniqChar = function(s) {
-    let hashmap = {};
-
-    for (let char of s) {
-        if (hashmap[char] === undefined) {
-            hashmap[char] = 1;
-        } else {
-            hashmap[char]++;
-        }
-    }
-
     for (let i = 0; i < s.length; i++) {
-        if (hashmap[s[i]] === 1) {
+        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
             return i;
         }
     }
-
-     return -1;
+    return -1;
 };
