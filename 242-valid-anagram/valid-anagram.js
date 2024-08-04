5c21d94d30d118775171c8fcc4aa381be05627bf
Tim Afanasiev <84281349+devnyxie@users.noreply.github.com>
2024-08-04 14:38:27 +0200

    Time: 55 ms (96.73%) | Memory: 50.4 MB (94.25%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,11 +4,21 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    function sortString(str) {
-        return str.split('').sort().join("");
+    if (s.length !== t.length) return false;
+
+    const charCountA = new Array(26).fill(0);
+
+    for (let i = 0; i < s.length; i++) {
+        charCountA[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
     }
-    if(sortString(s) == sortString(t)){
-        return true
+
+    for (let i = 0; i < t.length; i++) {
+        const index = t.charCodeAt(i) - 'a'.charCodeAt(0);
+        if (charCountA[index] === 0) {
+            return false; 
+        }
+        charCountA[index]--;
     }
-    return false
+
+    return true;
 };
